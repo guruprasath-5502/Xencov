5fab9cae99a79781d11d41ef5cce9899d4343b92
Guru Prasath <109198921+guruprasath-5502@users.noreply.github.com>
2023-04-02 17:34:23 +0530

    Add files via upload

---
+++ Server.js
@@ -0,0 +1,20 @@
+const express = require("express");
+require("dotenv").config();
+const mongoose = require("mongoose");
+const app = express();
+const userRoutes = require("./routes/userRoute");
+
+app.use(express.json());
+
+mongoose
+  .connect(process.env.MONGO_URI)
+  .then(() => {
+    app.listen(process.env.PORT, () => {
+      console.log(`DB Connected and Listening to Port : ${process.env.PORT}`);
+    });
+  })
+  .catch((err) => {
+    console.log(err);
+  });
+
+app.use("/users", userRoutes);
